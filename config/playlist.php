39ecf5c7d543ef51bc431884037b99c380afbf44
francoism90 <francoism90@users.noreply.github.com>
2025-07-25 11:13:23 +0000

    Add detailed comments for playlist configuration settings to improve clarity and maintainability

--- config/playlist.php
+++ config/playlist.php
@@ -39,9 +39,30 @@ return [
         // ['name' => 'ultra', 'bit_rate' => 9000],
     ],
 
+    /**
+     * This setting is used to configure the segment length for the playlist.
+     * The segment length is the duration of each segment in seconds.
+     * A shorter segment length will result in more segments, which can improve seeking performance,
+     * but will also increase the file size.
+     */
+
     'segment_length' => (int) env('PLAYLIST_SEGMENT_LENGTH', 10),
 
+    /**
+     * This setting is used to configure the frame interval for the playlist.
+     * The frame interval is the number of frames between each keyframe in the video.
+     * A lower frame interval will result in more keyframes, which can improve seeking performance,
+     * but will also increase the file size.
+     */
+
     'frame_interval' => (int) env('PLAYLIST_FRAME_INTERVAL', 48),
+
+    /**
+     * This setting is used to configure the video formats that will be used for transcoding.
+     * You can define multiple formats that are supported by the FFMpeg library.
+     * The order of the formats will determine the priority of transcoding.
+     * The first format in the list will be used as the default format for transcoding.
+     */
 
     'video_formats' => [
         \Support\FFMpeg\Format\Video\X264::class,
@@ -49,9 +70,25 @@ return [
         \Support\FFMpeg\Format\Video\WebM::class,
     ],
 
+    /**
+     * When this setting is true, the playlist will not transcode the video files.
+     * This will ignore any video format settings and will use the original video file as is.
+     * This is useful for cases where the original video file is already in a compatible format.
+     */
+
     'prevent_transcoding' => (bool) env('PLAYLIST_PREVENT_TRANSCODING', true),
 
+    /**
+     * When this setting is true, the playlist will copy the video codec from the original video file.
+     * This is useful for cases where the original video file is already in a compatible video codec.
+     */
+
     'copy_video_codec' => (bool) env('PLAYLIST_COPY_VIDEO_CODEC', true),
+
+    /**
+     * When this setting is true, the playlist will copy the audio codec from the original video file.
+     * This is useful for cases where the original video file is already in a compatible audio codec.
+     */
 
     'copy_audio_codec' => (bool) env('PLAYLIST_COPY_AUDIO_CODEC', true),
 
