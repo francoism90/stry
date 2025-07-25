5086cb3e4c9dd8292e6efd6f58cfc80aad9ed990
francoism90 <francoism90@users.noreply.github.com>
2025-07-25 08:31:11 +0000

    Remove favicon links from app layout for cleaner HTML structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="color-scheme" content="dark">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('storage/images/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/images/favicon-16x16.png') }}">
-<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="{{ asset('storage/images/apple-touch-icon.png') }}">
 @vite('resources/js/app.ts')
 @inertiaHead
 @googlefonts
