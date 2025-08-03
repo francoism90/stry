4e2604d655fbdb70d7739382c7a87d9638cb1c3f
francoism90 <francoism90@users.noreply.github.com>
2025-08-03 09:04:12 +0000

    Refactor temporary file paths: update temporary file paths in laravel-ffmpeg and media-library configurations to use sys_get_temp_dir() for improved portability.

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -16,8 +16,8 @@ return [
 
     'log_channel' => env('LOG_CHANNEL', 'stack'),
 
-    'temporary_files_root' => env('FFMPEG_TEMPORARY_FILES_ROOT', '/dev/shm'),
+    'temporary_files_root' => env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir()),
 
-    'temporary_files_encrypted_hls' => env('FFMPEG_TEMPORARY_ENCRYPTED_HLS', '/dev/shm'),
+    'temporary_files_encrypted_hls' => env('FFMPEG_TEMPORARY_ENCRYPTED_HLS', sys_get_temp_dir()),
 
 ];
--- config/media-library.php
+++ config/media-library.php
@@ -171,7 +171,7 @@ return [
      * The path where to store temporary files while performing image conversions.
      * If set to null, storage_path('media-library/temp') will be used.
      */
-    'temporary_directory_path' => env('FFMPEG_TEMPORARY_FILES_ROOT', '/dev/shm'),
+    'temporary_directory_path' => env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir()),
 
     /*
      * The engine that should perform the image conversions.
