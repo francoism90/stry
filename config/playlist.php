e6a562a4e3cf9fedac56e3f17b0d8afbd12b8428
François M. <francoism90@users.noreply.github.com>
2025-08-13 09:26:04 +0200

    Increase rotation key sections

--- config/playlist.php
+++ config/playlist.php
@@ -114,7 +114,7 @@ return [
      * Each section will have its own key, which will be used to encrypt the segments of the playlist.
      * A lower number of sections will result in more keys, which can decrease performance.
      */
-    'rotation_keys_sections' => (int) env('PLAYLIST_ROTATION_KEYS_SECTIONS', 5),
+    'rotation_keys_sections' => (int) env('PLAYLIST_ROTATION_KEYS_SECTIONS', 10),
 
     /**
      * This setting is used to define the disk where the rotation keys will be stored.
