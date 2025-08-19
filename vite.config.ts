88260d37dbf3268f8ea6612f414b51949b9827bb
francoism90 <francoism90@users.noreply.github.com>
2025-08-19 15:44:41 +0000

    fix: refactor Vite configuration to load environment variables dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -6,74 +6,79 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import laravel from 'laravel-vite-plugin'
 import { fileURLToPath, URL } from 'node:url'
 import { vite as vidstack } from 'vidstack/plugins'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    hmr: { host: 'vite.stry.test', clientPort: 443, protocol: 'wss' },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
-      '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
-      '!': fileURLToPath(new URL('./vendor', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const host = new URL(env.VITE_APP_URL)?.hostname || 'stry.test'
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      hmr: { host: `vite.${host}`, clientPort: 443, protocol: 'wss' },
     },
-  },
-  plugins: [
-    laravel({
-      input: ['resources/js/app.ts'],
-      ssr: 'resources/js/ssr.ts',
-      refresh: true,
-    }),
-    wayfinder(),
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('media-'),
-        },
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+        '!': fileURLToPath(new URL('./vendor', import.meta.url)),
       },
-    }),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
-    vidstack(),
-    ui({
-      inertia: true,
-      ui: {
-        colors: {
-          primary: 'purple',
-          secondary: 'gray',
-          neutral: 'zinc',
+    },
+    plugins: [
+      laravel({
+        input: ['resources/js/app.ts'],
+        ssr: 'resources/js/ssr.ts',
+        refresh: true,
+      }),
+      wayfinder(),
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('media-'),
+          },
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
         },
-        container: {
-          base: 'max-w-6xl',
+      }),
+      vueJsx(),
+      vueDevTools(),
+      tailwindcss(),
+      vidstack(),
+      ui({
+        inertia: true,
+        ui: {
+          colors: {
+            primary: 'purple',
+            secondary: 'gray',
+            neutral: 'zinc',
+          },
+          container: {
+            base: 'max-w-6xl',
+          },
+          input: {
+            slots: {
+              root: 'w-full',
+            },
+          },
         },
-        input: {
-          slots: {
-            root: 'w-full',
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            http: ['axios'],
+            broadcast: ['pusher-js', 'laravel-echo'],
+            play: ['hls.js', 'vidstack'],
           },
         },
       },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          http: ['axios'],
-          broadcast: ['pusher-js', 'laravel-echo'],
-          play: ['hls.js', 'vidstack'],
-        },
-      },
     },
-  },
+  }
 })
