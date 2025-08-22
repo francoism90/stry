256ae2dc289b3d68f6f49089da30203148e56e8a
francoism90 <francoism90@users.noreply.github.com>
2025-08-22 10:05:48 +0000

    fix: update condition in watchEffect to check state instead of data length

--- resources/js/composables/taginput.ts
+++ resources/js/composables/taginput.ts
@@ -24,7 +24,7 @@ export function useTagInput(selected?: MaybeRefOrGetter<Tag[]>) {
   watchEffect(async () => {
     items.value = toValue(selected || [])
 
-    if (!data.value?.length) {
+    if (!state.value) {
       await query()
     }
   })
