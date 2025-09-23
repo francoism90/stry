d47681b0fcdf11a3b6e7319abcc8781a7648c9ee
francoism90 <francoism90@users.noreply.github.com>
2025-09-23 16:01:57 +0000

    refactor: wrap video player template in PageSection component

--- resources/js/components/Video/VideoPlayer.vue
+++ resources/js/components/Video/VideoPlayer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import PageSection from '@/components/Ui/PageSection.vue'
 import { usePlayer } from '@/composables/player'
 import { useThrottleFn } from '@vueuse/core'
 import type { MediaPlayer } from 'vidstack'
@@ -8,7 +9,7 @@ import { onBeforeUnmount, onMounted, ref, shallowRef } from 'vue'
 const player = shallowRef<MediaPlayer>()
 const seeked = ref(false)
 
-const { state, src, captions, progress, record } = usePlayer()
+const { src, captions, progress, record } = usePlayer()
 
 const sessionHandler = useThrottleFn((time: number | null) => record(time), 2500)
 
@@ -31,32 +32,34 @@ onBeforeUnmount(() => listener())
 </script>
 
 <template>
-  <div
-    v-if="!src"
-    class="grid min-h-52 w-full place-items-center rounded-xl bg-neutral-800 md:min-h-96"
-  >
-    <span class="text-muted">Please wait while we prepare the video...</span>
-  </div>
+  <PageSection>
+    <div
+      v-if="!src"
+      class="grid min-h-52 w-full place-items-center rounded-xl bg-neutral-800 md:min-h-96"
+    >
+      <span class="text-muted">Please wait while we prepare the video...</span>
+    </div>
 
-  <media-player
-    ref="player"
-    .src="src || undefined"
-    .autoPlay="true"
-    .playsInline="true"
-    crossOrigin="anonymous"
-    class="rounded-xl"
-  >
-    <media-video-layout />
-    <media-provider>
-      <template v-if="captions?.length">
-        <track
-          v-for="caption in captions"
-          :key="caption.id"
-          :src="caption.asset"
-          :label="caption.name || 'undefined'"
-          kind="captions"
-        />
-      </template>
-    </media-provider>
-  </media-player>
+    <media-player
+      ref="player"
+      .src="src || undefined"
+      .autoPlay="true"
+      .playsInline="true"
+      crossOrigin="anonymous"
+      class="rounded-xl"
+    >
+      <media-video-layout />
+      <media-provider>
+        <template v-if="captions?.length">
+          <track
+            v-for="caption in captions"
+            :key="caption.id"
+            :src="caption.asset"
+            :label="caption.name || 'undefined'"
+            kind="captions"
+          />
+        </template>
+      </media-provider>
+    </media-player>
+  </PageSection>
 </template>
