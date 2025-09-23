d249e6d8a625616b42ddcb16c04d66214fe165d9
francoism90 <francoism90@users.noreply.github.com>
2025-09-23 15:12:32 +0000

    feat: create AppHeader component and integrate it into DefaultLayout

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -1,74 +1,15 @@
 <script lang="ts" setup>
-import AppLogo from '@/components/Ui/AppLogo.vue'
+import AppHeader from '@/components/Ui/AppHeader.vue'
 import ServiceWorker from '@/components/Ui/ServiceWorker.vue'
-import type { NavigationMenuItem } from '@nuxt/ui'
-import { computed } from 'vue'
-
-const items = computed<NavigationMenuItem[]>(() => [
-  {
-    label: 'Videos',
-    icon: 'i-lucide-gallery-vertical-end',
-    to: '/videos',
-    ui: {
-      linkLabel: 'sr-only',
-    },
-  },
-  {
-    label: 'Lists',
-    icon: 'i-lucide-library',
-    to: '/tags',
-    ui: {
-      linkLabel: 'sr-only',
-    },
-  },
-  {
-    label: 'Search',
-    icon: 'i-lucide-search',
-    to: '/search',
-    ui: {
-      linkLabel: 'sr-only',
-    },
-  },
-  {
-    label: 'Profile',
-    icon: 'i-lucide-user',
-    to: '/profile',
-    ui: {
-      linkLabel: 'sr-only',
-    },
-  },
-])
 </script>
 
 <template>
   <UApp>
     <ServiceWorker />
 
-    <div class="relative flex h-dvh min-h-dvh flex-col overflow-x-hidden">
-      <UHeader
-        :toggle="false"
-        class="sticky inset-x-0 top-0 z-30 flex h-18 min-h-18 border-b border-default bg-default"
-      >
-        <template #title>
-          <AppLogo />
-        </template>
-
-        <template #right>
-          <UNavigationMenu
-            variant="link"
-            :items="items"
-            :ui="{
-              list: 'inline-flex items-center gap-4',
-              link: 'px-0',
-              linkTrailingIcon: 'hidden',
-            }"
-          />
-        </template>
-      </UHeader>
-
-      <UMain class="flex-1 contain-inline-size contain-layout contain-paint contain-style">
-        <slot />
-      </UMain>
-    </div>
+    <UMain class="contain-inline-size contain-layout contain-paint contain-style">
+      <AppHeader />
+      <slot />
+    </UMain>
   </UApp>
 </template>
