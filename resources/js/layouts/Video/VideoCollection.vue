c90810baa59a2579f0344fe80f6e98110cf34d1b
francoism90 <francoism90@users.noreply.github.com>
2025-10-11 12:50:24 +0000

    fix: update grid gap styles in TagCollection and VideoCollection for improved layout

--- resources/js/layouts/Video/VideoCollection.vue
+++ resources/js/layouts/Video/VideoCollection.vue
@@ -21,7 +21,7 @@ defineProps<Props>()
     >
       <UPageGrid
         id="page-grid"
-        class="gap-4 py-2"
+        class="gap-x-4 gap-y-6 py-2"
       >
         <VideoCard
           v-for="item in items.data"
