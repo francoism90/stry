6a67bb9a2691f586bdcd2a8d271d82d3f22a15ea
francoism90 <francoism90@users.noreply.github.com>
2025-10-11 11:35:06 +0000

    fix: update default label in VideoTypeCollection for improved clarity

--- src/App/Web/Videos/Responses/VideoTypeCollection.php
+++ src/App/Web/Videos/Responses/VideoTypeCollection.php
@@ -13,7 +13,7 @@ readonly class VideoTypeCollection implements ProvidesInertiaProperty
     public function toInertiaProperty(PropertyContext $context): mixed
     {
         return [
-            ['value' => null, 'label' => __('All')],
+            ['value' => null, 'label' => __('Default')],
             ...VideoType::options(),
         ];
     }
