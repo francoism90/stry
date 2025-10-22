69959c0da28f6f0cab1379871fa8f7eedd9fc177
francoism90 <francoism90@users.noreply.github.com>
2025-10-22 11:46:43 +0000

    fix: update temporary directory path for image conversions to use system temp directory

--- config/media-library.php
+++ config/media-library.php
@@ -171,7 +171,7 @@ return [
      * The path where to store temporary files while performing image conversions.
      * If set to null, storage_path('media-library/temp') will be used.
      */
-    'temporary_directory_path' => env('MEDIA_CONVERSION_PATH', '/dev/shm'),
+    'temporary_directory_path' => env('MEDIA_CONVERSION_PATH', sys_get_temp_dir()),
 
     /*
      * The engine that should perform the image conversions.
