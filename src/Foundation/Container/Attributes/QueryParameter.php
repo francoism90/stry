407667b01edfd6b8ac7c07390a1adf44e73593e4
francoism90 <francoism90@users.noreply.github.com>
2025-10-29 21:42:52 +0000

    fix: remove default value handling from QueryParameter constructor and resolve method

--- src/Foundation/Container/Attributes/QueryParameter.php
+++ src/Foundation/Container/Attributes/QueryParameter.php
@@ -14,10 +14,7 @@ use Illuminate\Contracts\Container\ContextualAttribute;
 #[Attribute(Attribute::TARGET_PARAMETER)]
 class QueryParameter implements ContextualAttribute
 {
-    public function __construct(
-        public string $parameter,
-        public mixed $default = null,
-    ) {}
+    public function __construct(public string $parameter) {}
 
     /**
      * Resolve the query parameter from the request.
@@ -28,6 +25,6 @@ class QueryParameter implements ContextualAttribute
      */
     public static function resolve(self $attribute, Container $container): mixed
     {
-        return $container->make('request')->query($attribute->parameter, $attribute->default);
+        return $container->make('request')->query($attribute->parameter);
     }
 }
