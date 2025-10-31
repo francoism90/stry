060b1a02772dafc91e14727db2eee995fd151d7d
francoism90 <francoism90@users.noreply.github.com>
2025-10-31 15:32:40 +0000

    feat: update TagEdit component layout to include DashboardLayout and enhance TagEditProperties with TagType options

--- src/App/Web/Tags/Responses/TagEditProperties.php
+++ src/App/Web/Tags/Responses/TagEditProperties.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Web\Tags\Responses;
 
 use App\Api\Tags\Resources\TagResource;
+use Domain\Tags\Enums\TagType;
 use Domain\Tags\Models\Tag;
 use Illuminate\Container\Attributes\RouteParameter;
 use Inertia\ProvidesInertiaProperties;
@@ -20,6 +21,7 @@ readonly class TagEditProperties implements ProvidesInertiaProperties
     {
         return [
             'tag' => fn () => $this->tag->append('description', 'type', 'related')->toResource(TagResource::class),
+            'types' => fn () => TagType::options(),
         ];
     }
 }
