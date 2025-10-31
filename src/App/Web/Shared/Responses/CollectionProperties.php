61506075dbb3d0b5fbdad7ce1f1e9c476e77141f
francoism90 <francoism90@users.noreply.github.com>
2025-10-31 16:38:29 +0000

    fix(CollectionProperties): swap order of constructor parameters for consistency in query handling

--- src/App/Web/Shared/Responses/CollectionProperties.php
+++ src/App/Web/Shared/Responses/CollectionProperties.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace App\Web\Shared\Responses;
 
 use Foundation\Container\Attributes\QueryParameter;
-use Illuminate\Container\Attributes\RouteParameter;
 use Inertia\ProvidesInertiaProperties;
 use Inertia\RenderContext;
 
 class CollectionProperties implements ProvidesInertiaProperties
 {
     public function __construct(
-        #[RouteParameter('search')] protected ?string $search = null,
         #[QueryParameter('filter')] protected ?string $filter = null,
+        #[QueryParameter('search')] protected ?string $search = null,
     ) {}
 
     public function toInertiaProperties(RenderContext $context): array
