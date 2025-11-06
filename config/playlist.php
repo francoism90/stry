31c2ba56e3da676c8d19e24bddc400a05abcc52c
francoism90 <francoism90@users.noreply.github.com>
2025-11-06 17:27:46 +0000

    fix: comment out unsupported video formats in playlist configuration

--- config/playlist.php
+++ config/playlist.php
@@ -81,7 +81,8 @@ return [
     'video_formats' => [
         \Support\FFMpeg\Format\Video\X264::class,
         \Support\FFMpeg\Format\Video\X265::class,
-        \Support\FFMpeg\Format\Video\AV1::class,
+        // \Support\FFMpeg\Format\Video\AV1::class,
+        // \Support\FFMpeg\Format\Video\WebM::class,
     ],
 
     /**
