f40dd213a778703d7968ab427a406470ec356c6d
francoism90 <francoism90@users.noreply.github.com>
2025-11-06 22:06:59 +0000

    feat: update UserSettings data object to allow nullable properties and enhance fromModel method for user settings retrieval

--- src/App/Web/Shared/Responses/CollectionProperties.php
+++ src/App/Web/Shared/Responses/CollectionProperties.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App\Web\Shared\Responses;
 
+use Domain\Users\DataObjects\UserSettings;
+use Domain\Users\Models\User;
 use Foundation\Container\Attributes\QueryParameter;
+use Illuminate\Container\Attributes\CurrentUser;
 use Inertia\ProvidesInertiaProperties;
 use Inertia\RenderContext;
 
 readonly class CollectionProperties implements ProvidesInertiaProperties
 {
     public function __construct(
+        #[CurrentUser] protected ?User $user = null,
         #[QueryParameter('filter')] protected ?string $filter = null,
         #[QueryParameter('search')] protected ?string $search = null,
         #[QueryParameter('grid')] protected ?string $grid = null,
@@ -18,6 +22,11 @@ readonly class CollectionProperties implements ProvidesInertiaProperties
 
     public function toInertiaProperties(RenderContext $context): array
     {
+        $grid = UserSettings::fromModel($this->user)->include('general')->toArray();
+
+        dd($grid);
+
+
         return [
             'filter' => fn () => $this->filter,
             'search' => fn () => $this->search,
