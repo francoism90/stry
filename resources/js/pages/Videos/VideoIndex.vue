bba46693c301ee60c4938cb292b729967234ef95
francoism90 <francoism90@users.noreply.github.com>
2025-11-27 17:15:46 +0000

    feat: refactor VideoIndex component to streamline layout and improve type definitions

--- resources/js/pages/Videos/VideoIndex.vue
+++ resources/js/pages/Videos/VideoIndex.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import VideoCard from '@/components/Video/VideoCard.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
-import VideoCollection from '@/layouts/Videos/VideoCollection.vue'
-import type { Videos } from '@/types'
+import type { VideoCollection } from '@/types'
 import { Head, InfiniteScroll } from '@inertiajs/vue3'
 
 interface Props {
-  items: Videos
+  items: VideoCollection
 }
 
-defineOptions({ layout: [DashboardLayout, VideoCollection] })
+defineOptions({ layout: DashboardLayout })
 
 defineProps<Props>()
 </script>
@@ -17,19 +16,33 @@ defineProps<Props>()
 <template>
   <Head title="Library" />
 
-  <UPage>
-    <InfiniteScroll
-      data="items"
-      items-element="#video-items"
-      :buffer="200"
-    >
-      <UBlogPosts id="video-items">
-        <VideoCard
-          v-for="item in items?.data"
-          :key="item.id"
-          :item="item"
-        />
-      </UBlogPosts>
-    </InfiniteScroll>
-  </UPage>
+  <UDashboardPanel id="videos">
+    <template #header>
+      <UDashboardNavbar title="Videos">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+
+        <template #right>
+          <!-- <CustomersAddModal /> -->
+        </template>
+      </UDashboardNavbar>
+    </template>
+
+    <template #body>
+      <InfiniteScroll
+        data="items"
+        items-element="#video-items"
+        :buffer="200"
+      >
+        <UBlogPosts id="video-items">
+          <VideoCard
+            v-for="item in items?.data"
+            :key="item.id"
+            :item="item"
+          />
+        </UBlogPosts>
+      </InfiniteScroll>
+    </template>
+  </UDashboardPanel>
 </template>
