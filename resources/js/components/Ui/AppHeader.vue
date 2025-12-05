be9a114d3ec81f0ffa8207869dc1fa53ec0d37e8
francoism90 <francoism90@users.noreply.github.com>
2025-12-05 16:59:05 +0000

    style: reduce height of header in AppHeader component

--- resources/js/components/Ui/AppHeader.vue
+++ resources/js/components/Ui/AppHeader.vue
@@ -21,7 +21,7 @@ const items: NavigationMenuItem[] = [
 </script>
 
 <template>
-  <div class="sticky top-0 z-50 flex h-20 w-full items-center justify-between gap-4 border-b border-muted bg-default px-4 sm:px-6">
+  <div class="sticky top-0 z-50 flex h-16 w-full items-center justify-between gap-4 border-b border-muted bg-default px-4 sm:px-6">
     <ULink to="/">
       <span class="font-serif text-sm font-bold text-neutral-400 uppercase">stry</span>
     </ULink>
