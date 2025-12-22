8e83bd435230b483f06fe50023d64c7a9aa7e93c
François M. <francoism90@users.noreply.github.com>
2025-12-22 15:26:49 +0100

    feat: frankenphp (#187)

--- config/octane.php
+++ config/octane.php
@@ -40,7 +40,7 @@ return [
     |
     */
 
-    'server' => env('OCTANE_SERVER', 'swoole'),
+    'server' => env('OCTANE_SERVER', 'frankenphp'),
 
     /*
     |--------------------------------------------------------------------------
