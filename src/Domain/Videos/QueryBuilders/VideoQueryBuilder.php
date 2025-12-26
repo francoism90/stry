b90abe02c18d855333c6d655f54c917ef83f8863
francoism90 <francoism90@users.noreply.github.com>
2025-12-26 14:40:36 +0000

    feat: enhance video query by ensuring distinct results and ordering by video ID

--- src/Domain/Videos/QueryBuilders/VideoQueryBuilder.php
+++ src/Domain/Videos/QueryBuilders/VideoQueryBuilder.php
@@ -65,6 +65,8 @@ class VideoQueryBuilder extends Builder
                 ->on('videos.id', '=', 'groupables.groupable_id')
                 ->where('groupables.groupable_type', 'video'),
             )
+            ->selectRaw('DISTINCT ON (videos.id) videos.*, groupables.updated_at')
+            ->orderBy('videos.id')
             ->orderByDesc('groupables.updated_at');
     }
 }
