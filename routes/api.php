ebb943fc9ae65e55ddf813af7d513681a1af2ff4
francoism90 <francoism90@users.noreply.github.com>
2025-12-26 14:49:51 +0000

    feat: reorder use statements for VideoGroupController in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 use App\Api\Authentication\Controllers\HomeController;
-use App\Api\Videos\Controllers\VideoGroupController;
 use App\Api\Playlists\Controllers\PlaylistEncryptionKeyController;
 use App\Api\Playlists\Controllers\PlaylistManifestController;
 use App\Api\Playlists\Controllers\PlaylistSessionController;
 use App\Api\Tags\Controllers\TagController;
+use App\Api\Videos\Controllers\VideoGroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->prefix('v1')->group(function () {
