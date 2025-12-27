bba37728012594730fdcfb61372e6a802333ef09
francoism90 <francoism90@users.noreply.github.com>
2025-12-27 22:22:42 +0000

    fix: update preserveState option in form submissions to boolean

--- resources/js/pages/Admin/Users/UserIndex.vue
+++ resources/js/pages/Admin/Users/UserIndex.vue
@@ -19,7 +19,7 @@ const form = useForm('get', '', {
 
 const onSubmit = () =>
   form.submit({
-    preserveState: 'errors',
+    preserveState: true,
     replace: true,
     only: ['items', 'search'],
     reset: ['items'],
