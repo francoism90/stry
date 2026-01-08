ee8aa3ca614b7a2fcf8167dc6f8ad84eec824523
francoism90 <francoism90@users.noreply.github.com>
2026-01-08 19:44:32 +0000

    fix: update laravel-shaka package to version 0.4.0 and add transport stream timestamp offset option in CreateNewVideoPlaylist.php

--- src/Domain/Videos/Actions/CreateNewVideoPlaylist.php
+++ src/Domain/Videos/Actions/CreateNewVideoPlaylist.php
@@ -65,7 +65,8 @@ class CreateNewVideoPlaylist
             // Configure HLS playlist settings
             $opener
                 ->withHlsMasterPlaylist($playlist->getFileName())
-                ->withSegmentDuration(Playlist::getSegmentDuration());
+                ->withSegmentDuration(Playlist::getSegmentDuration())
+                ->withOption('transport_stream_timestamp_offset_ms', 1000);
 
             // Add text tracks (captions) to the playlist if available
             $video->getCaptions()->each(fn (Media $caption) => $opener->addTextStream($caption->getPath(), $caption->file_name, [
