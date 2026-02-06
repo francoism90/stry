4830a2a3f6d2603dc4a3410a1b1daa872cf5d9f1
francoism90 <francoism90@users.noreply.github.com>
2026-02-06 20:16:13 +0000

    refactor: remove unused imports from InteractsWithPlaylists trait for cleaner code

--- src/Domain/Playlists/Concerns/InteractsWithPlaylists.php
+++ src/Domain/Playlists/Concerns/InteractsWithPlaylists.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\Playlists\Concerns;
 
-use ArrayAccess;
 use Domain\Playlists\Collections\PlaylistCollection;
-use Domain\Playlists\Enums\PlaylistType;
 use Domain\Playlists\Models\Playlist;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
