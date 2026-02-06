249c6c9614994817b60c15d4837941fa22b6f226
francoism90 <francoism90@users.noreply.github.com>
2026-02-06 14:06:35 +0000

    refactor: attach Shaka Player to video element during initialization

--- resources/js/composables/shaka.ts
+++ resources/js/composables/shaka.ts
@@ -39,13 +39,10 @@ export function useShaka(element?: MaybeRefOrGetter<HTMLMediaElement | undefined
     }
 
     // Create new Shaka Player
-    player.value = new shaka.Player()
+    player.value = new shaka.Player(el.value)
 
     // Apply configuration
     player.value.configure(config)
-
-    // Attach player to video element
-    await player.value.attach(el.value)
 
     // Add timeupdate listener
     el.value.addEventListener('timeupdate', onTimeUpdate)
