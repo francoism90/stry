f89c578feee2b8b580c527a5073e75f8c278ed27
francoism90 <francoism90@users.noreply.github.com>
2026-02-12 15:31:24 +0000

    feat: enhance BasicPreset configuration for improved CSP directives and host handling

--- src/Support/Csp/Presets/BasicPreset.php
+++ src/Support/Csp/Presets/BasicPreset.php
@@ -9,30 +9,43 @@ use Spatie\Csp\Directive;
 use Spatie\Csp\Keyword;
 use Spatie\Csp\Policy;
 use Spatie\Csp\Preset;
+use Spatie\Csp\Scheme;
 use Spatie\Csp\Value;
 
 class BasicPreset implements Preset
 {
     public function configure(Policy $policy): void
     {
-        $s3Url = Config::string('filesystems.disks.s3.url');
+        $host = $this->getHost();
+
+        logger($host);
 
         $policy
+            ->add(Directive::UPGRADE_INSECURE_REQUESTS, Value::NO_VALUE)
+            ->add(Directive::BLOCK_ALL_MIXED_CONTENT, Value::NO_VALUE)
+            ->add(Directive::CONNECT, ["https://*.{$host}", "wss://*.{$host}"])
+            ->add([Directive::FRAME, Directive::SCRIPT, Directive::FONT, Directive::STYLE, Directive::IMG], "*.{$host}")
             ->add(Directive::BASE, Keyword::SELF)
-            ->add(Directive::DEFAULT, Keyword::SELF)
             ->add(Directive::CONNECT, Keyword::SELF)
+            ->add(Directive::CONNECT, 'api.iconify.design')
+            ->add(Directive::DEFAULT, Keyword::SELF)
+            ->add(Directive::FONT, Keyword::SELF)
+            ->add(Directive::FORM_ACTION, Keyword::SELF)
+            ->add(Directive::FRAME, Keyword::SELF)
+            ->add(Directive::IMG, Keyword::SELF)
+            ->add(Directive::MEDIA, Keyword::SELF)
+            ->add(Directive::MEDIA, Scheme::BLOB)
+            ->add(Directive::OBJECT, Keyword::NONE)
+            ->add(Directive::SCRIPT, Keyword::STRICT_DYNAMIC)
             ->add(Directive::SCRIPT, Keyword::SELF)
+            ->add(Directive::SCRIPT, Keyword::UNSAFE_INLINE)
             ->add(Directive::STYLE, Keyword::SELF)
             ->add(Directive::STYLE, Keyword::UNSAFE_INLINE)
-            ->add(Directive::STYLE, $s3Url)
-            ->add(Directive::IMG, Keyword::SELF)
-            // ->add(Directive::IMG, Keyword::DATA)
-            ->add(Directive::IMG, $s3Url)
-            ->add(Directive::FONT, Keyword::SELF)
-            ->add(Directive::FONT, $s3Url)
-            ->add(Directive::MEDIA, Keyword::SELF)
-            ->add(Directive::OBJECT, Keyword::NONE)
-            ->add(Directive::FORM_ACTION, Keyword::SELF)
-            ->add(Directive::FRAME, Keyword::SELF);
+            ->addNonce(Directive::SCRIPT);
+    }
+
+    protected function getHost(): string
+    {
+        return parse_url(Config::string('app.url', 'localhost'), PHP_URL_HOST);
     }
 }
