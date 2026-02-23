44884e2d65318edad76fc99e5fdf945c7e644e84
francoism90 <francoism90@users.noreply.github.com>
2026-02-23 09:24:07 +0000

    fix: refine condition for adding polymorphic fields in toSearchableArray method

--- src/Domain/Groups/Models/Groupable.php
+++ src/Domain/Groups/Models/Groupable.php
@@ -78,7 +78,7 @@ class Groupable extends MorphPivot
         ];
 
         // Add polymorphic fields if the relationship exists
-        if ($this->groupable_type && $this->groupable_id) {
+        if ($this->groupable->exists()) {
             $array["{$this->groupable_type}_id"] = (string) $this->groupable_id;
         }
 
