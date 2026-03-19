3a35cec62281436caa81baadb46f7c5534ed6cef
François M. <francoism90@users.noreply.github.com>
2026-03-19 12:39:49 +0100

    Feat icons (#288)

---
+++ resources/js/plugins/iconify/index.ts
@@ -0,0 +1,4 @@
+import lucideIcons from '@iconify-json/lucide/icons.json'
+import { addCollection } from '@iconify/vue'
+
+addCollection(lucideIcons)
--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,20 +1,12 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import createServer from '@inertiajs/vue3/server'
-import { configureEcho } from '@laravel/echo-vue'
 import ui from '@nuxt/ui/vue-plugin'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, h, type DefineComponent } from 'vue'
 import { renderToString } from 'vue/server-renderer'
 
-configureEcho({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT,
-  wssPort: import.meta.env.VITE_REVERB_PORT,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-  enabledTransports: ['ws', 'wss'],
-})
+import '@/plugins/echo'
+import '@/plugins/iconify'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
