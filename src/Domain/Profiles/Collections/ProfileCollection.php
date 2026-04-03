76dd1550f1ad39edb04d0f7da0e0d87888998d47
francoism90 <francoism90@users.noreply.github.com>
2026-04-03 13:13:03 +0000

    refactor(ProfileCollection): remove unused toSwitcherArray method

--- src/Domain/Profiles/Collections/ProfileCollection.php
+++ src/Domain/Profiles/Collections/ProfileCollection.php
@@ -4,16 +4,9 @@ declare(strict_types=1);
 
 namespace Domain\Profiles\Collections;
 
-use Domain\Profiles\Models\Profile;
 use Illuminate\Database\Eloquent\Collection;
 
 class ProfileCollection extends Collection
 {
-    public function toSwitcherArray(string $currentProfile = ''): array
-    {
-        return $this
-            ->map(fn (Profile $profile): array => $profile->toSwitcherArray($currentProfile))
-            ->values()
-            ->all();
-    }
+    //
 }
