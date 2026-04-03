1617b4135a76cf5e46846ff2c1e10108f98d8566
francoism90 <francoism90@users.noreply.github.com>
2026-04-03 14:13:55 +0000

    fix(account): change redirect to return back after password update

--- src/App/Web/Account/Responses/PasswordUpdateResponse.php
+++ src/App/Web/Account/Responses/PasswordUpdateResponse.php
@@ -23,6 +23,6 @@ class PasswordUpdateResponse implements PasswordUpdateResponseContract
             'type' => 'success',
         ]);
 
-        return redirect()->route('security');
+        return back();
     }
 }
