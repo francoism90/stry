be888981c88950d14cb7915eeb1a3e80c85d41c3
francoism90 <francoism90@users.noreply.github.com>
2026-04-03 12:52:26 +0000

    feat(profiles): implement profile deletion logic in HasProfiles trait and update morph map in AppServiceProvider

--- src/Domain/Profiles/Concerns/HasProfiles.php
+++ src/Domain/Profiles/Concerns/HasProfiles.php
@@ -5,10 +5,25 @@ declare(strict_types=1);
 namespace Domain\Profiles\Concerns;
 
 use Domain\Profiles\Models\Profile;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 trait HasProfiles
 {
+    public static function bootHasProfiles(): void
+    {
+        static::deleting(function (Model $model) {
+            if (in_array(SoftDeletes::class, class_uses_recursive($model))) {
+                if (! $model->forceDeleting) {
+                    return;
+                }
+            }
+
+            $model->profiles()->cursor()->each(fn (Profile $profile) => $profile->delete());
+        });
+    }
+
     public function profiles(): HasMany
     {
         return $this->hasMany(Profile::class)->chaperone();
@@ -16,8 +31,7 @@ trait HasProfiles
 
     public function currentProfile(): ?Profile
     {
-        return $this
-            ->profiles()
+        return $this->profiles()
             ->current()
             ->first();
     }
