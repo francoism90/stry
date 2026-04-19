bb198ef5e31f54d092edcb2f65ae950d7c31dcaf
francoism90 <francoism90@users.noreply.github.com>
2026-04-19 15:33:58 +0000

    refactor: cast environment variables to appropriate types and update temporary files root path

--- config/streamer.php
+++ config/streamer.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'force_generic_input' => env('STREAMER_FORCE_GENERIC_INPUT', true),
+    'force_generic_input' => (bool) env('STREAMER_FORCE_GENERIC_INPUT', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'timeout' => env('STREAMER_TIMEOUT', 14400),
+    'timeout' => (int) env('STREAMER_TIMEOUT', 14400),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,7 +60,7 @@ return [
     |
     */
 
-    'temporary_files_root' => env('STREAMER_TEMPORARY_FILES_ROOT', '/cache/temp/streamer'),
+    'temporary_files_root' => env('STREAMER_TEMPORARY_FILES_ROOT', storage_path('app/streamer/temp')),
 
     /*
     |--------------------------------------------------------------------------
