a5a001afc0dc204c4621504faae68c9044c3b559
francoism90 <francoism90@users.noreply.github.com>
2026-04-20 08:54:01 +0000

    refactor: wrap configureEcho call in SSR check

--- resources/js/plugins/echo/index.ts
+++ resources/js/plugins/echo/index.ts
@@ -1,11 +1,13 @@
 import { configureEcho } from '@laravel/echo-vue'
 
-configureEcho({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT,
-  wssPort: import.meta.env.VITE_REVERB_PORT,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-  enabledTransports: ['ws', 'wss'],
-})
+if (!import.meta.env.SSR) {
+  configureEcho({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+  })
+}
