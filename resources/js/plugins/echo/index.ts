a9963deb98ab7618f4d21e1328a56e6d83c4b28e
francoism90 <francoism90@users.noreply.github.com>
2026-04-20 11:17:48 +0000

    refactor: simplify configureEcho call with conditional operator

--- resources/js/plugins/echo/index.ts
+++ resources/js/plugins/echo/index.ts
@@ -1,13 +1,15 @@
 import { configureEcho } from '@laravel/echo-vue'
 
-if (!import.meta.env.SSR) {
-  configureEcho({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-  })
-}
+configureEcho(
+  import.meta.env.SSR
+    ? { broadcaster: 'null' }
+    : {
+        broadcaster: 'reverb',
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT,
+        wssPort: import.meta.env.VITE_REVERB_PORT,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+        enabledTransports: ['ws', 'wss'],
+      },
+)
