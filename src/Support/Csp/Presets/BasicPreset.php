5db761b7ceb13182e249c1898befb9f8b6f50aed
francoism90 <francoism90@users.noreply.github.com>
2026-06-16 17:21:18 +0000

    fix: remove BLOCK_ALL_MIXED_CONTENT directive and redundant SCRIPT directives from CSP policy

--- src/Support/Csp/Presets/BasicPreset.php
+++ src/Support/Csp/Presets/BasicPreset.php
@@ -23,7 +23,6 @@ class BasicPreset implements Preset
         // Configure the CSP policy with directives and sources
         $policy
             ->add(Directive::UPGRADE_INSECURE_REQUESTS, Value::NO_VALUE)
-            ->add(Directive::BLOCK_ALL_MIXED_CONTENT, Value::NO_VALUE)
             ->add(Directive::BASE, Keyword::SELF)
             ->add(Directive::CONNECT, Keyword::SELF)
             ->add(Directive::DEFAULT, Keyword::SELF)
@@ -37,12 +36,10 @@ class BasicPreset implements Preset
             ->add(Directive::MEDIA, Scheme::BLOB)
             ->add(Directive::OBJECT, Keyword::NONE)
             ->add(Directive::SCRIPT, Keyword::STRICT_DYNAMIC)
-            ->add(Directive::SCRIPT, Keyword::SELF)
-            ->add(Directive::SCRIPT, Keyword::UNSAFE_INLINE)
             ->add(Directive::STYLE, Keyword::SELF)
             ->add(Directive::STYLE, Keyword::UNSAFE_INLINE)
             ->add(Directive::CONNECT, ["https://*.{$host}", "wss://*.{$host}"])
-            ->add([Directive::FRAME, Directive::SCRIPT, Directive::FONT, Directive::STYLE, Directive::IMG], "*.{$host}")
+            ->add([Directive::FRAME, Directive::FONT, Directive::STYLE, Directive::IMG], "*.{$host}")
             ->addNonce(Directive::SCRIPT);
     }
 
