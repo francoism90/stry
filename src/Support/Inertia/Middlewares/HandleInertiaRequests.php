944ed5580f0bac81f5b21efdb709ac8901c2f236
francoism90 <francoism90@users.noreply.github.com>
2026-06-17 22:19:43 +0000

    wip

--- src/Support/Inertia/Middlewares/HandleInertiaRequests.php
+++ src/Support/Inertia/Middlewares/HandleInertiaRequests.php
@@ -28,7 +28,7 @@ class HandleInertiaRequests extends Middleware
                 appends: ['name', 'email', 'avatar', 'settings'])
             ),
             'echo' => Inertia::once(fn (): array => [
-                'key' => Config::string('reverb.apps.apps.0.wsKey', ''),
+                'key' => Config::string('reverb.apps.apps.0.options.wsKey', ''),
                 'host' => Config::string('reverb.apps.apps.0.options.wsHost', 'localhost'),
                 'port' => Config::integer('reverb.apps.apps.0.options.wsPort', 6001),
                 'scheme' => Config::string('reverb.apps.apps.0.options.wsScheme', 'http'),
