e587fdb0531dfdf1e9f82d84b91645a38edb8f8e
francoism90 <francoism90@users.noreply.github.com>
2026-07-28 20:15:54 +0000

    refactor: reorder layout imports for improved clarity

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,14 +1,14 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import ui from '@nuxt/ui/vue-plugin'
 
+import AppLayout from '@/layouts/AppLayout.vue'
+import ResourceLayout from '@/layouts/Resources/ResourceLayout.vue'
+import AuthLayout from './layouts/AuthLayout.vue'
+
 import '@/plugins/iconify'
 import '@/plugins/pusher'
 
 import '../css/app.css'
-
-import AppLayout from '@/layouts/AppLayout.vue'
-import ResourceLayout from '@/layouts/Resources/ResourceLayout.vue'
-import AuthLayout from './layouts/AuthLayout.vue'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
