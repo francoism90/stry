9e4b4eb56559d1f5a4fdfcbf16ae6eba47b181fe
francoism90 <francoism90@users.noreply.github.com>
2026-07-28 15:12:20 +0000

    refactor: add Collection components for improved organization and functionality

--- resources/js/pages/App/Collections/CollectionIndex.vue
+++ resources/js/pages/App/Collections/CollectionIndex.vue
@@ -1,48 +1,31 @@
 <script setup lang="ts">
-import GroupCreateModal from '@/components/Groups/GroupCreateModal.vue'
-import GroupFilterBar from '@/components/Groups/GroupFilterBar.vue'
-import GroupList from '@/components/Groups/GroupList.vue'
+import CollectionList from '@/components/Collections/CollectionList.vue'
 import AppFooter from '@/components/Ui/AppFooter.vue'
 import AppHeader from '@/components/Ui/AppHeader.vue'
 import type { GroupCollection } from '@/types'
 import { InfiniteScroll } from '@inertiajs/vue3'
-import type { SelectMenuItem } from '@nuxt/ui'
 
 defineProps<{
   items: GroupCollection
-  sorters: SelectMenuItem[]
-  sort?: string | null
 }>()
 </script>
 
 <template>
   <UDashboardPanel id="collections">
     <template #header>
-      <AppHeader />
+      <AppHeader>
+        <FilterBar placeholder="Search collections" />
+      </AppHeader>
     </template>
 
     <template #body>
       <UPage>
-        <UDashboardToolbar>
-          <template #left>
-            <GroupFilterBar
-              :results="Boolean(items?.data?.length)"
-              :sorters="sorters"
-              :sort="sort"
-            />
-          </template>
-
-          <template #right>
-            <GroupCreateModal />
-          </template>
-        </UDashboardToolbar>
-
         <InfiniteScroll
           data="items"
           items-element="#infinite-items"
           :buffer="200"
         >
-          <GroupList
+          <CollectionList
             id="infinite-items"
             :items="items?.data"
           />
