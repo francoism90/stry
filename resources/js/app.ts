27c5c04dffdb8963e461c306022adac1b1bb35e1
francoism90 <francoism90@users.noreply.github.com>
2026-07-29 07:52:38 +0000

    refactor: simplify layout selection logic by removing ResourceLayout integration

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,7 +2,6 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import ui from '@nuxt/ui/vue-plugin'
 
 import AppLayout from '@/layouts/AppLayout.vue'
-import ResourceLayout from '@/layouts/Resources/ResourceLayout.vue'
 import AuthLayout from './layouts/AuthLayout.vue'
 
 import '@/plugins/iconify'
@@ -15,14 +14,11 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 createInertiaApp({
   title: (title) => (title ? `${title} - ${appName}` : appName),
   layout: (name) => {
-    switch (true) {
-      case name.startsWith('Auth/'):
-        return AuthLayout
-      case name.startsWith('Resources/'):
-        return [AppLayout, ResourceLayout]
-      default:
-        return AppLayout
+    if (name.startsWith('Auth/')) {
+      return AuthLayout
     }
+
+    return AppLayout
   },
   progress: false,
   withApp(app) {
