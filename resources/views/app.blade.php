151fb0d5d34c51e5e36d490cc77ca74a98677748
francoism90 <francoism90@users.noreply.github.com>
2026-08-03 13:31:26 +0000

    fix: add prettier-plugin-blade for improved Blade template formatting

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,22 @@
 <!doctype html>
 <html class="scroll-smooth" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="color-scheme" content="dark">
-<meta name="referrer" content="strict-origin-when-cross-origin">
-<link rel="preconnect" href="{{ config('filesystems.disks.s3.url') }}" crossorigin="anonymous">
-@pwaHead
-@fonts
-@vite('resources/js/app.ts')
-<x-inertia::head />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="color-scheme" content="dark" />
+    <meta name="referrer" content="strict-origin-when-cross-origin" />
+    <link rel="preconnect" href="{{ config('filesystems.disks.s3.url') }}" crossorigin="anonymous" />
+    @pwaHead
+    @fonts
+    @vite('resources/js/app.ts')
+    <x-inertia::head />
 </head>
 
 <body class="antialiased">
-
     <div class="isolate">
         <x-inertia::app />
     </div>
 
     @pwaSw
-
 </body>
 </html>
