4518a10d97e78c2e597c917a023630c9c601cdff
francoism90 <francoism90@users.noreply.github.com>
2026-08-03 11:44:51 +0000

    fix: format query method in VideoProfileScope for improved readability

--- src/Domain/Videos/Scopes/VideoProfileScope.php
+++ src/Domain/Videos/Scopes/VideoProfileScope.php
@@ -12,9 +12,15 @@ readonly class VideoProfileScope
 {
     public function __invoke(Builder $scout): void
     {
+<<<<<<< HEAD
         $scout
             ->query(fn (VideoQueryBuilder $query) => $query
                 ->with(['media', 'tags'])
                 ->forProfile(Profile::current()));
+=======
+        $scout->query(fn (VideoQueryBuilder $query) => $query
+            ->with(['media', 'tags'])
+            ->forProfile(Profile::current()));
+>>>>>>> 402807bf (fix: format query method in VideoProfileScope for improved readability)
     }
 }
