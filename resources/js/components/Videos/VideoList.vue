f8a73eff9c913ff7951e6c2eb0e81e52be6c87d6
francoism90 <francoism90@users.noreply.github.com>
2026-08-03 12:12:58 +0000

    fix: adjust gap classes in TagList and VideoList components for consistent layout

--- resources/js/components/Videos/VideoList.vue
+++ resources/js/components/Videos/VideoList.vue
@@ -9,10 +9,14 @@ defineProps<{
 
 <template>
 <<<<<<< HEAD
+<<<<<<< HEAD
   <UBlogPosts class="gap-6 lg:gap-y-8 xl:grid-cols-4">
 =======
   <UBlogPosts class="grid grid-cols-1 gap-4 gap-y-6 sm:grid-cols-3 xl:grid-cols-4">
 >>>>>>> caaed9fe (fix: refactor tag and video list components to use dynamic color and update layout structure)
+=======
+  <UBlogPosts class="grid grid-cols-1 gap-4 gap-y-6 sm:grid-cols-3 lg:gap-y-8 xl:grid-cols-4">
+>>>>>>> a20dc31c (fix: adjust gap classes in TagList and VideoList components for consistent layout)
     <VideoCard
       v-for="(item, index) in items"
       :key="item.id"
