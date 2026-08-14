c1460be8c9a131c15e3076764f57d6d155897d7d
francoism90 <francoism90@users.noreply.github.com>
2026-08-14 21:47:48 +0000

    feat: enhance FilterBar component layout and update select menu variants for improved UI

--- resources/js/components/Ui/FilterBar.vue
+++ resources/js/components/Ui/FilterBar.vue
@@ -31,7 +31,7 @@ watchDebounced(
   <UDashboardToolbar>
     <UFormField
       :error="form.errors.query"
-      class="min-w-3/4 flex-1"
+      class="flex-1"
     >
       <UInput
         v-model="form.query"
@@ -44,37 +44,43 @@ watchDebounced(
       />
     </UFormField>
 
-    <USelectMenu
-      v-if="filters?.length"
-      v-model="formFilters"
-      :model-modifiers="{ nullable: true }"
-      :items="filters"
-      :search-input="false"
-      :ui="{ content: 'min-w-40' }"
-      placeholder="Filters"
-      label-key="label"
-      value-key="value"
-      variant="ghost"
-      size="lg"
-      multiple
-      clear
-      @update:modelValue="onSubmit"
-    />
+    <UFormField :error="form.errors.sort">
+      <USelectMenu
+        v-if="sorters?.length"
+        v-model="form.sort"
+        :model-modifiers="{ nullable: true }"
+        :items="sorters"
+        :search-input="false"
+        :ui="{ content: 'min-w-40' }"
+        placeholder="Sort by"
+        label-key="label"
+        value-key="value"
+        variant="outline"
+        icon="i-lucide-list-sort-descending"
+        size="lg"
+        clear
+        @update:modelValue="onSubmit"
+      />
+    </UFormField>
 
-    <USelectMenu
-      v-if="sorters?.length"
-      v-model="form.sort"
-      :model-modifiers="{ nullable: true }"
-      :items="sorters"
-      :search-input="false"
-      :ui="{ content: 'min-w-40' }"
-      placeholder="Sort by"
-      label-key="label"
-      value-key="value"
-      variant="ghost"
-      size="lg"
-      clear
-      @update:modelValue="onSubmit"
-    />
+    <UFormField :error="form.errors.filter">
+      <USelectMenu
+        v-if="filters?.length"
+        v-model="formFilters"
+        :model-modifiers="{ nullable: true }"
+        :items="filters"
+        :search-input="false"
+        :ui="{ content: 'min-w-40' }"
+        placeholder="Filters"
+        label-key="label"
+        value-key="value"
+        variant="outline"
+        icon="i-lucide-sliders"
+        size="lg"
+        multiple
+        clear
+        @update:modelValue="onSubmit"
+      />
+    </UFormField>
   </UDashboardToolbar>
 </template>
